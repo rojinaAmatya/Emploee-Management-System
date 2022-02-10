db85ce5c4d6fe992c9ab0cac37f400269d4d76ea
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-10 16:43:12 -0600

    created data access objects in interface

---
+++ src/com/ems/dao/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package com.ems.dao;
+
+import com.ems.pojo.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public void addEmployee();
+    List<Employee> viewEmployees();
+    Employee viewEmployee(int emp_id);
+    void updateEmployee();
+    void deleteEmployee();
+}
+
