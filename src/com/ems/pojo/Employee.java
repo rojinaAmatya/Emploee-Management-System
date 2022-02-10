3bc17ac8d9f12712aeb493c104760f37adadb53c
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-10 16:37:12 -0600

    created Employee class

--- src/com/ems/pojo/Employee.java
+++ src/com/ems/pojo/Employee.java
@@ -1,4 +1,52 @@
 package com.ems.pojo;
 
 public class Employee {
+
+    private int emp_id;
+    private String emp_name;
+    private String emp_address;
+    private String emp_email;
+
+    public Employee(int emp_id, String emp_name, String emp_address, String emp_email) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_address = emp_address;
+        this.emp_email = emp_email;
+    }
+
+    public Employee() {
+        super();
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getEmp_name() {
+        return emp_name;
+    }
+
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
+    }
+
+    public String getEmp_address() {
+        return emp_address;
+    }
+
+    public void setEmp_address(String emp_address) {
+        this.emp_address = emp_address;
+    }
+
+    public String getEmp_email() {
+        return emp_email;
+    }
+
+    public void setEmp_email(String emp_email) {
+        this.emp_email = emp_email;
+    }
 }
